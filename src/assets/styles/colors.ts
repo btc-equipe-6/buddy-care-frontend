bb46dae4acc2c186460bf3f6298034ce019086b4
Manoel Rocha <manoelrocha399@gmail.com>
2023-03-08 19:49:25 -0300

    chore: configuration styled components

---
+++ src/assets/styles/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+    primaryColor: '#5CC4E9',
+    secondaryColor: '#34AEDA',
+    backgroudColor: '#D9D9D9',
+}
---
+++ src/assets/styles/constants.ts
@@ -0,0 +1,13 @@
+export const constants = {
+    logoFontFamily: 'Poppins',
+    logoFontSize: '50px',
+    logoLineHeight: '140%',
+
+    headingFontFamily: 'Poppins',
+    headingFontSize: '28px',
+    headingLineHeight: '140%',
+
+    bodyFontFamily: 'Poppins',
+    bodyFontSize: '14px',
+    bodyLineHeight: '140%',
+};
