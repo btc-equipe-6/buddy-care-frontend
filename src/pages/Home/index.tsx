53af17abbee740952c6d14ad35dab6a07b4aa38c
Manoel Rocha <manoelrocha399@gmail.com>
2023-03-09 17:17:32 -0300

    feat(Home): home dashboard structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,20 @@
+import { BottomBox, ChartBox, ChartBoxTwo, Container, Content, TopBox } from "./styles";
+
 const Home = () => {
     return ( 
-        <div>
-        </div>
+        <Container>
+            <Content>
+                <TopBox>
+                    <ChartBox/>
+                    <ChartBox/>
+                    <ChartBox/>
+                </TopBox>
+                <BottomBox>
+                    <ChartBoxTwo/>
+                    <ChartBoxTwo/>
+                </BottomBox>
+            </Content>  
+        </Container>
      );
 }
  
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,62 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem 4rem;
+  height: 100vh;
+  width: 100%;
+  max-width: 1920px;
+  overflow: hidden;
+  background: linear-gradient(90deg, rgba(244,244,244,1) 4%, rgba(112,194,187,1) 100%);
+  color: black;
+  @media screen and (max-width: 1280px) {
+    padding: 2rem 2rem;
+  }
+  @media screen and (max-width: 1080px) {
+    padding: 2rem 1rem;
+  }
+`
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+export const TopBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
+export const BottomBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
+export const ChartBox = styled.div`
+  display: flex;
+  width: 17rem;
+  height: 15rem;
+  margin: 0.3rem;
+  background-color: #ffffffff;
+  border-radius: 10%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
+export const ChartBoxTwo = styled.div`
+  display: flex;
+  width: 27rem;
+  height: 15rem;
+  margin: 0.3rem;
+  background-color: #ffffffff;
+  border-radius: 10%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
+
