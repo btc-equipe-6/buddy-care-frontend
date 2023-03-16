da07af5a8872746607e6011c78bc04881862430c
Manoel Rocha <manoelrocha399@gmail.com>
2023-03-16 17:05:49 -0300

    feat(Patients): patient page creation

---
+++ src/pages/Patients/index.tsx
@@ -0,0 +1,21 @@
+import PatientCard from "components/PatientCards";
+import { Container, InputPatient, PatientContainer } from "./styles";
+import { useState } from "react";
+import { patients } from "components/PatientCards";
+
+const Patients  = () => {
+  const [filter, setFilter] = useState("");
+  const filteredPatients = patients.filter((patient) =>
+  patient.name.toLowerCase().includes(filter.toLowerCase())
+);
+    return ( 
+        <Container>
+          <InputPatient value={filter} onChange={(e) => setFilter(e.target.value)} />
+            <PatientContainer>
+                <PatientCard filteredPatients={filteredPatients} />
+            </PatientContainer>
+        </Container>
+     );
+}
+ 
+export default Patients ;
---
+++ src/pages/Patients/styles.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem 4rem;
+  height: 100%;
+  width: 100%;
+  gap: 1rem;
+  max-width: 1920px;
+  overflow: hidden;
+  background: linear-gradient(90deg, rgba(244,244,244,1) 4%, rgba(112,194,187,1) 100%);
+  color: black;
+  @media screen and (max-width: 1280px) {
+    padding: 2rem 2rem;
+  }
+  @media screen and (max-width: 1080px) {
+    padding: 2rem 1rem;
+  }
+`
+
+export const PatientContainer = styled.div`
+  display: grid;
+  width: 51rem;
+  height: 50%;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+`
+export const InputPatient = styled.input`
+  width: 51rem;
+  height: 2.5rem;
+  border-radius: 1.25rem;
+  border: 0.125rem solid #ccc;
+  background-color: #f8f8f8;
+  padding: 0.625rem;
+  font-size: 1rem;
+  margin-bottom: 1.25rem;
+`
+
+ 
