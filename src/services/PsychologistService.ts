66bb578cca96d5efb61cb1f32091f853eb0d6ee9
Manoel Rocha <manoelrocha399@gmail.com>
2023-03-30 13:15:02 -0300

    refactor: refactoring create psychologist

--- src/services/PsychologistService.ts
+++ src/services/PsychologistService.ts
@@ -6,16 +6,7 @@ import {
  PsychologistDetails
 } from 'types/api-types/psychologist'
 
-export const AllPsychologist = {
- AllPsychologists: async () => {
-  try {
-   const res = await Api.get('/psychologist', { method: 'GET' })
-   return res
-  } catch (error: any) {
-   console.log(error)
-  }
- }
-}
+
 
 export function PsychologistService() {
  async function create(data: InputCreatePsychologist) {
