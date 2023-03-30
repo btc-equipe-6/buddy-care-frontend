65da6b00de2577667da1783642db41f4fc01925f
jjribeiro1 <jvsr.99@hotmail.com>
2023-03-29 22:55:38 -0300

    feat: psychologist service and types

--- src/services/PsychologistService.ts
+++ src/services/PsychologistService.ts
@@ -1,12 +1,55 @@
-import Api from "helpers/Api/axios-config";
+import Api from 'helpers/Api/axios-config'
+import {
+ InputCreatePsychologist,
+ InputUpdatePsychologist,
+ Psychologist,
+ PsychologistDetails
+} from 'types/api-types/psychologist'
 
 export const AllPsychologist = {
-  AllPsychologists: async () => {
-    try {
-      const res = await Api.get("/psychologist", { method: "GET" });
-      return res;
-    } catch (error: any) {
-      console.log(error);
-    }
-  },
-};
+ AllPsychologists: async () => {
+  try {
+   const res = await Api.get('/psychologist', { method: 'GET' })
+   return res
+  } catch (error: any) {
+   console.log(error)
+  }
+ }
+}
+
+export function PsychologistService() {
+ async function create(data: InputCreatePsychologist) {
+  const { name, email, password, crp, phoneNumber } = data
+  const request = await Api.post<Psychologist>('psychologist', {
+   name,
+   email,
+   password,
+   crp,
+   phoneNumber
+  })
+
+  return request.data
+ }
+
+ async function findAll() {
+  const request = await Api.get<Psychologist[]>('psychologist')
+  return request.data
+ }
+
+ async function findById(id: string) {
+  const request = await Api.get<PsychologistDetails>(`psychologist/${id}`)
+  return request.data
+ }
+
+ async function update(id: string, data: InputUpdatePsychologist) {
+  const request = await Api.patch<Psychologist>(`psychologist/${id}`, { data })
+  return request.data
+ }
+
+ return {
+  create,
+  findAll,
+  findById,
+  update,
+ }
+}
--- src/types/api-types/psychologist.ts
+++ src/types/api-types/psychologist.ts
@@ -1,7 +1,37 @@
 export interface Psychologist {
-    name: string;
-    email: string;
-    password: string;
-    crp: string;
-    phoneNumber: string;
+ id: string
+ name: string
+ email: string
+ crp: string
+ phoneNumber: string
+ role: string
+ university?: string
+ age?: number
+ specialization?: string
+ bio?: string
+ state?: string
+ city?: string
+ street?: string
+ fee?: number
 }
+
+export interface PsychologistDetails extends Psychologist {
+ patients?: any[]
+ availability?: any[]
+ therapySession?: any[]
+ therapySessionRequest?: any[]
+ sentimentAnalysisPacient?: any[]
+ sentimentAnalysisPsychologist?: any[]
+ sentimentAnalysisEvolution?: any[]
+}
+
+export interface InputCreatePsychologist {
+ name: string
+ email: string
+ password: string
+ crp: string
+ phoneNumber: string
+}
+
+export interface InputUpdatePsychologist
+ extends Partial<Omit<Psychologist, 'id' | 'role'>> {}
